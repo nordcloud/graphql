ddca0d5b1b66d39291e2137da804e0ad3c145ac0
kevwargo <jarasz.kevwargo@gmail.com>
2019-07-29 10:14:27 +0200

    Fix: check HTTP response code before trying to decode JSON (#2)

--- graphql.go
+++ graphql.go
@@ -137,10 +137,10 @@ func (c *Client) runWithJSON(ctx context.Context, req *Request, resp interface{}
 		return errors.Wrap(err, "reading body")
 	}
 	c.logf("<< %s", buf.String())
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("graphql: server returned a non-200 status code: %v", res.StatusCode)
+	}
 	if err := json.NewDecoder(&buf).Decode(&gr); err != nil {
-		if res.StatusCode != http.StatusOK {
-			return fmt.Errorf("graphql: server returned a non-200 status code: %v", res.StatusCode)
-		}
 		return errors.Wrap(err, "decoding response")
 	}
 	if len(gr.Errors) > 0 {
